f2453ff5fa82a66724757eb7648f7193397a705c
raghu <raghumanbabu@gmail.com>
2023-10-08 12:24:47 +0200

    Added the main classes for strcutured concurrency.

---
+++ Hackathon1/src/com/raghutrainingdeep11/session2/Main.java
@@ -0,0 +1,9 @@
+package com.raghutrainingdeep11.session2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello 2 from structured concurrency");
+    }
+
+}
